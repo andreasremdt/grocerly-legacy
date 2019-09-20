e321706dc5eb01131281a3dfa9ad7fb3c7fe5852
Andreas Remdt <andreas.remdt@gmail.com>
2019-09-20 20:08:30 +0200

    clear service worker cache

--- sw.js
+++ sw.js
@@ -34,3 +34,15 @@ self.addEventListener("fetch", function(evt) {
     })
   );
 });
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
